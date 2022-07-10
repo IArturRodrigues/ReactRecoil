bd44759f857a85859ab545d4e58e557941602a3d
Artur Rodrigues <artrodrigues19@gmail.com>
2022-07-10 05:06:29 -0300

    4.2 adicionando filtro por data

--- src/components/EventList/index.tsx
+++ src/components/EventList/index.tsx
@@ -1,19 +1,31 @@
 import { useEventsValue } from '@states/events/eventsAtomHooks';
+import { useFilterValue } from '@states/events/filterAtomHooks';
 
 import Event from '../Event';
 import Filter from '../Filter';
 
 import { Scroll } from './EventList';
 
+const LENGTH_DATE_FORMAT = 10;
+
 function EventList () {
    const events = useEventsValue();
+   const filter = useFilterValue();
+
+   const eventsUsingFilter = events.filter(event => {
+      if (!filter.date) {
+         return true;
+      }
+      const isSameDay = filter.date.toISOString().slice(0, LENGTH_DATE_FORMAT) === event.startedAt.toISOString().slice(0, LENGTH_DATE_FORMAT);
+      return isSameDay;
+   });
 
    return (
       <section>
          <Filter />
          <Scroll>
             {
-               events.map(event => (
+               eventsUsingFilter.map(event => (
                   <Event event={event} key={event.id} />
                ))
             }
